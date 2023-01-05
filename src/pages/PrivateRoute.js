f847fd3ed947c71567efad4e307f813605fb56b5
Ammar Kasem <linkamar@gmail.com>
2023-01-05 16:13:40 +0100

    updated dependency file

---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Navigate } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user } = useAuth0()
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return user ? children : <Navigate to="/"></Navigate>
+      }}
+    ></Route>
+  )
+}
+export default PrivateRoute
