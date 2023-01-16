35e766a49e3b289bda574231ccb494280d4864cc
Ammar Kasem <linkamar@gmail.com>
2023-01-16 10:31:40 +0100

    changed currecy to kr

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,21 +1,14 @@
 export const formatPrice = (number) => {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(number / 100)
-}
-// export const formatPrice = (number) => {
-//   const newNumber = (number / 100).toLocaleString('en-NG', {
-//     style: 'currency',
-//     currency: 'NGN',
-//   })
-//   return newNumber
-// }
+  return new Intl.NumberFormat("sv-SW", {
+    style: "currency",
+    currency: "SEK",
+  }).format(number / 100);
+};
 
 export const getUniqueValues = (data, type) => {
-  let unique = data.map((item) => item[type])
-  if (type === 'colors') {
-    unique = unique.flat()
+  let unique = data.map((item) => item[type]);
+  if (type === "colors") {
+    unique = unique.flat();
   }
-  return ['all', ...new Set(unique)]
-}
+  return ["all", ...new Set(unique)];
+};
